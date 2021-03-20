ea46dc09312cd6dd601e04d0af3be8ffe1ba69e4
Bald Mike <baldmike312@gmail.com>
2021-03-19 22:10:23 -0500

    adds vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
+import Vue from 'vue' 
+import VueRouter from 'vue-router'
+import { router } from './router'
+
 require('./bootstrap');
+Vue.use(VueRouter)
+
+window.Vue = require('vue');
+
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+window.addEventListener('load', function () { 
+    const app = new Vue({
+        el: '#app',
+        template: '<router-view></router-view>',
+        router,
+    })
+})
